4474b2f82ab2b5cd4419afbd75582737979b7fb2
Roy Montoya <roymontoya89@gmail.com>
2018-03-06 13:56:23 -0600

    created second activity

---
+++ app/src/main/java/com/example/roy/habittrainer/CreateHabitActivity.kt
@@ -0,0 +1,83 @@
+package com.example.roy.habittrainer
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_create_habit.*
+import java.io.IOException
+
+class CreateHabitActivity : AppCompatActivity() {
+
+    private val TAG = CreateHabitActivity::class.java.simpleName
+    private val CHOOSE_IMAGE_REQUEST = 1;
+    private var choosenBitmap: Bitmap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_habit)
+    }
+
+    fun chooseImage(v: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
+    }
+
+    fun saveHabit(v: View) {
+        if (et_title.isBlank() || et_description.isBlank()) {
+            Log.d(TAG, "Missing title or description")
+            displayError("Your habit needs a title and description ")
+            return
+        } else if (choosenBitmap == null) {
+            Log.d(TAG, "Missing image")
+            displayError("add a picture for you habit")
+            return
+        }
+
+        tv_error.visibility = View.INVISIBLE
+    }
+
+    private fun displayError(errorDescription: String) {
+        tv_error.text = errorDescription
+        tv_error.visibility = View.VISIBLE
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+                && data != null && data.data != null) {
+
+            val bitmap = tryReadBitmap(data.data)
+
+
+            bitmap?.let {
+                choosenBitmap = bitmap
+                iv_image_preview.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun tryReadBitmap(data: Uri): Bitmap? {
+        return try {
+            MediaStore.Images.Media.getBitmap(contentResolver, data)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+}
+
+private fun EditText.isBlank() = this.text.toString().isBlank()
--- app/src/main/java/com/example/roy/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/roy/habittrainer/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.roy.habittrainer
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,4 +18,22 @@ class MainActivity : AppCompatActivity() {
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = HabitsAdapter(getSampleHabits())
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_habit) {
+            switchTo(CreateHabitActivity::class.java)
+        }
+        return false
+    }
+
+    private fun switchTo(c: Class<*>) {
+        val intent = Intent(this, c)
+        startActivity(intent)
+    }
+
 }
