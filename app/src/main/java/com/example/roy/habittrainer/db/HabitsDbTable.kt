ddac427a4eff05b5b03aadf75e0fbac2bf8fc6d4
Roy Montoya <roymontoya89@gmail.com>
2018-03-07 17:24:42 -0600

    added missing methods

--- app/src/main/java/com/example/roy/habittrainer/db/HabitsDbTable.kt
+++ app/src/main/java/com/example/roy/habittrainer/db/HabitsDbTable.kt
@@ -1,5 +1,13 @@
 package com.example.roy.habittrainer.db
 
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.example.roy.habittrainer.Habit
+import java.io.ByteArrayOutputStream
+
 /**
  * Created by Roy on 3/6/18.
  */
@@ -8,8 +16,59 @@ class HabitsDbTable(context: Context) {
 
     private val dbHelper = HabitTrainerDb(context)
 
-    fun store() : Long
+    fun store(): Long {
 
 
+    }
 
+    fun readAllHabits(): List<Habit> {
+        val columns = arrayOf(HabitEntry._ID, HabitEntry.TITLE_COLUMN, HabitEntry.DESCRIPTION_COLUMN,
+                HabitEntry.IMAGE_COLUMN)
+
+        val order = "${HabitEntry._ID} ASC"
+
+        val db = dbHelper.readableDatabase
+
+        val cursor: Cursor = db.doQuery(HabitEntry.TABLE_NAME, columns, orderBy = order)
+
+        val habits = mutableListOf<Habit>()
+        while (cursor.moveToNext()) {
+            val title = cursor.getString(cursor.getColumnIndex(HabitEntry.TITLE_COLUMN))
+            val description = cursor.getString(cursor.getColumnIndex(HabitEntry.DESCRIPTION_COLUMN))
+            val byteArray = cursor.getBlob(cursor.getColumnIndex(HabitEntry.IMAGE_COLUMN))
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+            habits.add(Habit(title, description, bitmap))
+        }
+        cursor.close()
+
+        return habits
+    }
+
+
+    private fun toByteArray(bitmap: Bitmap): ByteArray {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
+        return stream.toByteArray()
+    }
+}
+
+
+private fun SQLiteDatabase.doQuery(table: String, columns: Array<String>, selection: String? = null,
+                                   selectionArgs: Array<String>? = null, groupBy: String? = null,
+                                   having: String? = null, orderBy: String? = null): Cursor {
+    return query(table, columns, selection, selectionArgs, groupBy, having, orderBy)
+}
+
+private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() -> T): T {
+    beginTransaction()
+    val result = try {
+        val returnValue = function()
+        setTransactionSuccessful()
+        returnValue
+    } finally {
+        endTransaction()
+    }
+    close()
+
+    return result
 }
